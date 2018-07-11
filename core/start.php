588104a1259bd3d934d5dc502770d77ddc080b02
Jeremy Lim <git@jerl.im>
2018-07-11 11:47:43 +0800

    Add user init section

--- core/start.php
+++ core/start.php
@@ -6,8 +6,11 @@ define("APP_PATH", __DIR__ . "/../src");
 // Use Composer autoloader
 require __DIR__ . "/../vendor/autoload.php";
 
-// load routes
+// Load routes
 require_once __DIR__ . "/../routes.php";
+
+// User declared initialisation
+require_once __DIR__ . "/../init_user.php";
 
 // Load appropriate route
 KeythKatz\Barrens\Router\Route::handleRoute();
---
+++ init_user.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+
+// Stuff that should be done on every startup should be put here
+
